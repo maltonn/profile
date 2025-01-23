78bed085872dcb9794f5639e8efdda407d5c032f
Maltonn <ymatsubayashi09@outlook.jp>
2025-01-24 00:19:15 +0900

    qrcode

---
+++ qr/main.js
@@ -0,0 +1,6 @@
+const size=Math.min(window.innerWidth,window.innerHeight-250)*0.9;
+var qrcode = new QRCode("output",{width:size,height:size});
+document.getElementById('btn').addEventListener('click', () => {
+    qrcode.clear();
+    qrcode.makeCode(document.getElementById('text').value.trim());
+})
