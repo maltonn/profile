8b95488f215478bfc359f3bd34e3dbd999396270
Maltonn <ymatsubayashi09@outlook.jp>
2025-01-24 22:08:07 +0900

    update qr/

--- qr/main.js
+++ qr/main.js
@@ -1,6 +1,11 @@
 const size=Math.min(window.innerWidth,window.innerHeight-250)*0.9;
 var qrcode = new QRCode("output",{width:size,height:size});
-document.getElementById('btn').addEventListener('click', () => {
+// document.getElementById('btn').addEventListener('click', () => {
+//     qrcode.clear();
+//     qrcode.makeCode(document.getElementById('text').value.trim());
+// })
+
+document.getElementById('text').addEventListener('input', () => {
     qrcode.clear();
     qrcode.makeCode(document.getElementById('text').value.trim());
 })
